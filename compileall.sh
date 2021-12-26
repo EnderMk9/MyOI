7d6ca70e1143df3cf029defe77a736b6c61e322b
Ender <abel.rosadop@gmail.com>
2021-12-26 18:10:59 +0100

    reinstall

--- compileall.sh
+++ compileall.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Compile document
-pdflatex -interaction=nonstopmode main
-pdflatex -interaction=nonstopmode main
+xelatex -interaction=nonstopmode main
+xelatex -interaction=nonstopmode main
 rm main.aux
 rm main.idx
 rm main.log
 rm main.mw
 rm main.toc
+rm main.ilg
+rm main.fls
+rm main.fdb_latexmk
